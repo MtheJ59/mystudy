4ff2f9f65065178d11b613afe80c46e81dc86cb8
MtheJ59 <mthej59@gmail.com>
2023-12-12 17:08:16 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/member/MemberModifyHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/member/MemberModifyHandler.java
@@ -21,15 +21,18 @@ public class MemberModifyHandler implements MenuHandler {
     System.out.printf(AnsiEscape.ANSI_BOLD + "[%s]\n" + AnsiEscape.ANSI_CLEAR, menu.getTitle());
 
     int index = this.prompt.inputInt("번호? ");
-    if (index < 0 || index >= this.memberRepository.length) {
+    Member oldmember = this.memberRepository.get(index);
+    if (oldmember == null) {
       System.out.println("회원 번호가 유효하지 않습니다.");
       return;
     }
 
-    Member member = this.memberRepository.members[index];
-    member.email = this.prompt.input("이메일(%s)? ", member.email);
-    member.name = this.prompt.input("이름(%s)? ", member.name);
+    Member member = new Member();
+    member.email = this.prompt.input("이메일(%s)? ", oldmember.email);
+    member.name = this.prompt.input("이름(%s)? ", oldmember.name);
     member.password = this.prompt.input("새 암호? ");
-    member.createdDate = this.prompt.input("가입일(%s)? ", member.createdDate);
+    member.createdDate = this.prompt.input("가입일(%s)? ", oldmember.createdDate);
+
+    this.memberRepository.set(index, member);
   }
 }
