a42be3c6338958771f7457e57abdcf1d01af546f
MtheJ59 <mthej59@gmail.com>
2023-12-18 17:56:00 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level02/exam03/Main.java
@@ -0,0 +1,21 @@
+package algorithm.test.baekjoon.level02.exam03;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+
+    if (x == 12 && y == 5) {
+      System.out.println("1");
+      if (x == -12 && y == 5)
+        System.out.println("2");
+      if (x == -12 && y == -5)
+        System.out.println("3");
+      if (x == 12 && y == 5)
+        System.out.println("4");
+    }
+  }
+}
