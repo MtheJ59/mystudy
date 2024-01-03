202f9cc69d2f7d30a6726cec8f4faaa9dcdfa9e5
MtheJ59 <mthej59@gmail.com>
2024-01-03 18:00:27 +0900

    .

--- myapp/app/src/main/java/bitcamp/menu/MenuGroup.java
+++ myapp/app/src/main/java/bitcamp/menu/MenuGroup.java
@@ -1,10 +1,10 @@
 package bitcamp.menu;
 
-import bitcamp.util.Iterator;
-import bitcamp.util.LinkedList;
-import bitcamp.util.List;
 import bitcamp.util.Prompt;
-import bitcamp.util.Stack;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 // Composite 패턴에서 '복합 객체(composite object)' 역할을 하는 클래스
 // - 다른 Menu 객체를 포함한다.
@@ -63,7 +63,7 @@ public class MenuGroup extends AbstractMenu {
     int i = 1;
     while (iterator.hasNext()) {
       Menu menu = iterator.next();
-      System.out.printf("%d. %s\n", (i++), menus.get(i).getTitle());
+      System.out.printf("%d. %s\n", i++, menu.getTitle());
     }
 
     System.out.printf("0. %s\n", "이전");
