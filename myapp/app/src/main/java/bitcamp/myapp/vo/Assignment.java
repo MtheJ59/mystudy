3b17a5eb2dcddfbf199e69722b9e548142604e84
MtheJ59 <mthej59@gmail.com>
2024-01-05 18:21:11 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/vo/Assignment.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Assignment.java
@@ -3,11 +3,28 @@ package bitcamp.myapp.vo;
 import java.io.Serializable;
 import java.sql.Date;
 
-public class Assignment implements Serializable {
+public class Assignment implements Serializable, CsvString {
+
+  private static final long serialVersionUID = 100L;
 
   private String title;
   private String content;
   private Date deadline;
+
+  // 팩토리 메소드
+  public static Assignment createFromCsv(String csv) {
+    String[] values = csv.split(","); // "aaa,aaaa,2023-01-01" 이면 배열에는 {'aaa,aaaa,2023-01-01'}
+    Assignment obj = new Assignment();
+    obj.setTitle(values[0]);
+    obj.setContent(values[1]);
+    obj.setDeadline((Date.valueOf(values[2])));
+    return obj;
+  }
+
+  @Override
+  public String toCsvString() {
+    return String.format("%s,%s,%s", this.title, this.content, this.deadline);
+  }
 
   public String getTitle() {
     return title;
