9a256eaf4fe75c50d77568d02b8de4f5400bb66d
MtheJ59 <MtheJ59@gmail.com>
2024-01-26 17:34:00 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam06/Main.java
@@ -0,0 +1,17 @@
+package algorithm.test.baekjoon.level03.exam06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int T = Integer.parseInt(br.readLine());
+
+    StringTokenizer st;
+  }
+}
