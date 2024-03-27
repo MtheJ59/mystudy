ed9d8f702a78f169c68b118df7bd4544cf29ac32
mthej59 <mthej59@gamil.com>
2024-03-27 16:41:12 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/service/StorageService.java
+++ myapp/app/src/main/java/bitcamp/myapp/service/StorageService.java
@@ -3,5 +3,5 @@ package bitcamp.myapp.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
-  String upload(MultipartFile multipartFile) throws Exception;
+  String upload(String bucketName, String path, MultipartFile multipartFile) throws Exception;
 }
