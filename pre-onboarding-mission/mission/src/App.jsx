3483b0ed6d7ad3eeace44516c8d729493f179b7e
mthej59 <mthej59@gamil.com>
2024-07-24 17:40:07 +0900

    mission

--- pre-onboarding-mission/mission/src/App.jsx
+++ pre-onboarding-mission/mission/src/App.jsx
@@ -30,37 +30,40 @@ function App() {
   }, {});
 
   return (
-    <div className="app">
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <div className="search-icon">🔍</div>
-      </div>
-      {searchTerm && (
-        <div className="results">
-          <div className="scrollable-results">
-            {Object.entries(groupedData).map(([type, items]) => (
-              <div key={type}>
-                <div className="category">{type}</div>
-                {items.map((item, index) => (
-                  <div
-                    key={index}
-                    className="result-item"
-                    onClick={() => handleItemClick(item.description)}
-                  >
-                    {highlightMatch(item.description, searchTerm)}
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
+    <>
+      <div className="line" />
+      <div className="app">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div className="search-icon">🔍</div>
         </div>
-      )}
-    </div>
+        {searchTerm && (
+          <div className="results">
+            <div className="scrollable-results">
+              {Object.entries(groupedData).map(([type, items]) => (
+                <div key={type}>
+                  <div className="category">{type}</div>
+                  {items.map((item, index) => (
+                    <div
+                      key={index}
+                      className="result-item"
+                      onClick={() => handleItemClick(item.description)}
+                    >
+                      {highlightMatch(item.description, searchTerm)}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
